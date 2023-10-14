4d02a5a73cbe0c5470783bde293cb3384f5ec11d
Michael Levesque-Dion <mlevesquedion@google.com>
2023-10-14 00:30:45 -0700

    Day 15

---
+++ day15.py
@@ -0,0 +1,30 @@
+import math
+import re
+
+discs = []
+start_time, step = 0, 1
+multiple = 1
+for i, line in enumerate(open('day15.txt'), 1):
+    _, positions, _, start = map(int, re.findall('\d+', line))
+    discs.append((positions, start))
+    if positions > step:
+        start_time = - ((start + i) % positions)
+        step = positions
+    if ((start + i) % positions == 0) and positions > multiple:
+        multiple = positions
+step = math.lcm(step, multiple)
+
+
+def find_time():
+    time = start_time
+    while True:
+        if all((start + time + i) % positions == 0 for i, (positions, start) in enumerate(discs, 1)):
+            break
+        time += step
+    return time
+
+
+print('part 1:', find_time())
+
+discs.append((11, 0))
+print('part 2:', find_time())
